a102824e4c19b12b7c818d8784ccdc135d36c891
KlutzyBubbles <LTzilantonis@gmail.com>
2023-12-10 23:57:37 +1100

    Supposedly v1.0.0?

---
+++ VBCan/Patches.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace VBCan
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(GrabbableObject), "SetScrapValue")]
+        [HarmonyPostfix]
+        private static void SetScrapValuePatch(GrabbableObject __instance)
+        {
+            if (__instance.itemProperties.itemName == "Red soda")
+            {
+                if (__instance.mainObjectRenderer != null)
+                {
+                    foreach (Material material in __instance.mainObjectRenderer.materials)
+                    {
+                        material.mainTexture = Plugin.texture;
+                    }
+                }
+                /*
+                 *Not sure if this is needed yet
+                if (__instance.itemProperties.spawnPrefab != null)
+                {
+                    StaticLogger.LogInfo($"not null");
+                    foreach (Material material in __instance.itemProperties.spawnPrefab.GetComponent<MeshRenderer>().materials)
+                    {
+                        material.mainTexture = Plugin.texture;
+                    }
+                }
+                */
+            }
+        }
+    }
+}
--- VBCan/Plugin.cs
+++ VBCan/Plugin.cs
@@ -3,19 +3,24 @@ using System;
 using System.IO;
 using UnityEngine;
 using HarmonyLib;
+using BepInEx.Logging;
+using UnityEngine.UIElements;
 
 namespace VBCan
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private static Texture2D texture;
+        public static Texture2D texture;
+
+        public static ManualLogSource StaticLogger;
 
         private void Awake()
         {
+            StaticLogger = Logger;
             try
             {
-                string path = Directory.GetFiles(Paths.PluginPath, "LethalVBCan/vb.png")[0];
+                string path = Directory.GetFiles(Path.Combine(Paths.PluginPath, PluginInfo.PLUGIN_NAME), "vb.png")[0];
                 texture = new Texture2D(2, 2);
                 texture.LoadImage(File.ReadAllBytes(path));
             }
@@ -23,34 +28,8 @@ namespace VBCan
             {
                 Logger.LogError($"Unable to load VB can texture with error: {e.Message}");
             }
+            new Harmony(PluginInfo.PLUGIN_GUID).PatchAll(typeof(Patches));
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
-
-        [HarmonyPatch(typeof(GrabbableObject), "SetScrapValue")]
-        [HarmonyPostfix]
-        private static void SetScrapValuePatch(GrabbableObject __instance)
-        {
-            if (__instance.itemProperties.itemName == "Red soda")
-            {
-                if (__instance.mainObjectRenderer != null)
-                {
-                    foreach (Material material in __instance.mainObjectRenderer.materials)
-                    {
-                        material.mainTexture = texture;
-                    }
-                }
-                /*
-                 * Not sure if this is needed yet
-                if (__instance.itemProperties.spawnPrefab != null)
-                {
-                    foreach (Material material in __instance.itemProperties.spawnPrefab.GetComponent<MeshRenderer>().materials)
-                    {
-                        material.mainTexture = Plugin.texture;
-                    }
-                }
-                */
-            }
-        }
-
     }
 }
